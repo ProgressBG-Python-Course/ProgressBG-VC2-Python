ab3c9a7280d37854d2ab1724213520f72f464451
WWWCourses <ivawebcourses@gmail.com>
2018-05-31 16:44:17 +0300

    transfer to X1C5

--- .bin/live-server.sh
+++ .bin/live-server.sh
@@ -1,5 +1,6 @@
 nohup live-server --port=3333 --open=/ProgressBG-VC2-Python/pages/agenda/agenda.html \
 --watch=/ProgressBG-VC2-Python   \
-/home/nemsys/projects/www/wwwcourses.github.io &
+/home/nemsys/projects/courses/ProgressBG/ &
+
 
 echo 'live-server started on port=3333 for ProgressBG-VC2-Python'
