ece5be479ba4bf3d6b82d60f3df0de5f1f993931
WWWCourses <ivawebcourses@gmail.com>
2018-06-22 18:41:14 +0300

    Unicode/encoding more examples

--- pages/themes/beginners/unicodeTopics/examples/fromWin1251_toUTF8.py
+++ pages/themes/beginners/unicodeTopics/examples/fromWin1251_toUTF8.py
@@ -2,23 +2,20 @@ input_file_name = "windows-1251_encoded_file.txt"
 output_file_name = "utf8_encoded_file.txt"
 
 
-def decode_from(enc, file_name):
-  # open file for read in bytemode
-  with open(file_name,"rb") as f:
-    content_in_bytes = f.read()
-    decoded_content = content_in_bytes.decode(enc)
+def read_from(enc, file_name):
+  # open file for read in textmode with encoding:
+  with open(file_name,"r", encoding="cp1251") as f:
+    decoded_content = f.read()
 
   return decoded_content
 
-def encode_to(enc, content, file_name):
-  # encode content:
-  encoded_content = content.encode(enc)
-  # open a file in byte write mode:
-  with open(file_name,"wb") as f:
-    f.write(encoded_content)
+def write_to(enc, content, file_name):
+  # open file for write in textmode with encoding:
+   with open(file_name,"w+", encoding=enc) as f:
+    f.write(content)
 
 
-decoded_content = decode_from("cp1251", input_file_name)
-encode_to("iso8859_5", decoded_content, output_file_name)
+decoded_content = read_from("cp1251", input_file_name)
+write_to("utf8", decoded_content, output_file_name)
 
 
