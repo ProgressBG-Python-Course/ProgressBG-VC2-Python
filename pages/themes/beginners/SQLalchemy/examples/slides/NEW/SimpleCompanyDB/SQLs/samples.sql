fa40301cb3706378d1a1debb3c51ee8e198993e8
WWWCourses <ivawebcourses@gmail.com>
2018-06-29 01:49:50 +0300

    mess

--- pages/themes/beginners/SQLalchemy/examples/slides/NEW/SimpleCompanyDB/SQLs/samples.sql
+++ pages/themes/beginners/SQLalchemy/examples/slides/NEW/SimpleCompanyDB/SQLs/samples.sql
@@ -4,9 +4,9 @@ SELECT
     company.company_id,
     employee.employee_name,
     company.company_name,
-FROM company
+FROM employee
 INNER JOIN company_employee ON employee.employee_id = company_employee.employee_id
-INNER JOIN employee ON company.company_id = company_employee.company_id
+INNER JOIN company ON company.company_id = company_employee.company_id
 
 -- select all employees working for google:
 SELECT
@@ -14,6 +14,6 @@ SELECT
     company.company_id,
     employee.employee_name,
     company.company_name,
-FROM product
+FROM company
 INNER JOIN company_employee ON employee.employee_id = company_employee.employee_id
 INNER JOIN employee ON company.company_id = company_employee.company_id
--- pages/themes/beginners/SQLalchemy/examples/slides/real/SimpleCompanyDB/auto_reflect.py
+++ pages/themes/beginners/SQLalchemy/examples/slides/real/SimpleCompanyDB/auto_reflect.py
@@ -1,41 +1,50 @@
-`from sqlalchemy import *
+# from sqlalchemy import *
+# for reflection:
+from sqlalchemy import  MetaData, Table
+from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine import reflection
 # from sqlalchemy.orm import create_session
 # from sqlalchemy.orm import contains_eager, joinedload
 # from sqlalchemy.orm import relationship
 # from datetime import datetime
 
 
-#Create the engine and get the metadata
+#Create the engine
 # Base = declarative_base()
-
 engine = create_engine(
-    'mysql+pymysql://pythontest:123@localhost/SimpleCompanyDB?charset=utf8',
+    'mysql+pymysql://demo:123@localhost/SimpleCompanyDB?charset=utf8',
     connect_args = {
         # 'port': 3306
     },
     # echo='debug',
     echo_pool=True
 )
-metadata = MetaData(bind=engine)
 
 
-# print table names:
-print( engine.table_names() )
+insp = reflection.Inspector.from_engine(engine)
+print(insp.get_table_names())
 
-#Reflect each database table we need to use, using metadata
-# class Company(Base):
-#     __table__ = Table('Company', metadata, autoload=True)
-#     orders = relationship("Order", backref="customer")
-
-company = Table('company', metadata, autoload=True, autoload_with=engine)
-employee = Table('employee', metadata, autoload=True, autoload_with=engine)
-for c in company.columns:
-  print(c)
+# print( insp.get_columns("employee"))
 
 
-# insert a record:
-ins = employee.insert().values(employee_name='Pesho')
-print(ins)
+# # get the metadata
+# metadata = MetaData(bind=engine)
+
+# # print table names:
+# print(f'tables: {engine.table_names()}')
+
+# #Reflect each database table we need to use, using metadata
+# company = Table('company', metadata, autoload=True, autoload_with=engine)
+# employee = Table('employee', metadata, autoload=True, autoload_with=engine)
+# print("Company table columns:")
+# for c in company.columns:
+#   print(c)
+
+# # print content of employees
+# print(repr(employee))
+# # insert a record:
+# ins = employee.insert().values(employee_name='Pesho')
+# print(ins)
 
 # Session = sessionmaker(bind=engine)
